6c9aad2860757c9e9a8cb4930cec0e477fcdddc7
marcosfaria19 <marcos.faria19@hotmail.com>
2024-10-15 14:20:04 -0300

    fix: aceitar batch requests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const UTMLatLng = require("utm-latlng"); // Certifique-se de que a biblioteca está corretamente importada
+const UTMLatLng = require("utm-latlng");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,30 +7,32 @@ const port = process.env.PORT || 3000;
 // Middleware para permitir JSON no corpo da requisição
 app.use(express.json());
 
-app.post("/convert", (req, res) => {
-  const { easting, northing, zone, hemisphere } = req.body;
-
-  console.log("Recebido:", req.body); // Verificar a requisição recebida
+app.post("/convert-batch", (req, res) => {
+  const data = req.body; // Espera-se que seja um array de objetos com easting, northing, zone, hemisphere
+  const results = [];
 
   try {
     // Instanciar a classe UTMLatLng
     const utmConverter = new UTMLatLng();
 
-    // Usar a nova biblioteca para converter UTM para Latitude/Longitude
-    const latLon = utmConverter.convertUtmToLatLng(
-      easting,
-      northing,
-      zone,
-      hemisphere
-    );
+    // Loop para processar todas as coordenadas no lote
+    data.forEach((item) => {
+      const { easting, northing, zone, hemisphere } = item;
+      const latLon = utmConverter.convertUtmToLatLng(
+        easting,
+        northing,
+        zone,
+        hemisphere
+      );
 
-    console.log("Resultado:", latLon); // Verificar resultado da conversão
-
-    // Retornar um objeto JSON com as propriedades de latitude e longitude
-    res.json({
-      latitude: latLon.lat,
-      longitude: latLon.lng,
+      results.push({
+        latitude: latLon.lat,
+        longitude: latLon.lng,
+      });
     });
+
+    // Retornar o array de resultados
+    res.json(results);
   } catch (error) {
     console.error("Erro:", error); // Log de erro
     res.status(400).json({ error: "Invalid UTM coordinates" });
@@ -38,5 +40,5 @@ app.post("/convert", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
